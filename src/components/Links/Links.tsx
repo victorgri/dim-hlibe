6335df076ecded7fe9bb3e7de80bfc780fff2446
Victor Hrygorash <viktor12041991@i.ua>
2024-11-27 11:00:18 +0200

    fix bugs

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -7,96 +7,96 @@ export const Links = () => {
       id="links-container"
     >
       <div className="h-full">
-        <div className="relative h-full" data-id="116646831">
-          <div
-            className="-top[92px] invisible relative block"
-            id="116646831"
-          ></div>
-          <div className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl">
-            <a
-              href="https://www.dom-hleba.org.ua/"
-              target="_blank"
-              rel="noopener"
-              data-testid="LinkButton"
-              className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
-              aria-describedby="profile-spasinnya "
-            >
+        <a
+          href="https://www.dom-hleba.org.ua/"
+          target="_blank"
+          rel="noopener"
+          data-testid="LinkButton"
+          className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
+          aria-describedby="profile-spasinnya "
+        >
+          <div className="relative h-full" data-id="116646831">
+            <div
+              className="-top[92px] invisible relative block"
+              id="116646831"
+            ></div>
+            <div className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl">
               Сайт церкви
-            </a>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
       <div className="h-full">
-        <div className="relative h-full" data-id="116647489">
-          <div
-            className="-top[92px] invisible relative block"
-            id="116647489"
-          ></div>
-          <div
-            data-testid="StyledContainer"
-            data-link
-            className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl"
-          >
-            <a
-              href="https://www.instagram.com/dim_hliba_kr?igsh=bDc1cmhmNXBxaTZu"
-              target="_blank"
-              rel="noopener"
-              data-testid="LinkButton"
-              className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
-              aria-describedby="profile-spasinnya "
+        <a
+          href="https://www.instagram.com/dim_hliba_kr?igsh=bDc1cmhmNXBxaTZu"
+          target="_blank"
+          rel="noopener"
+          data-testid="LinkButton"
+          className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
+          aria-describedby="profile-spasinnya "
+        >
+          <div className="relative h-full" data-id="116647489">
+            <div
+              className="-top[92px] invisible relative block"
+              id="116647489"
+            ></div>
+            <div
+              data-testid="StyledContainer"
+              data-link
+              className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl"
             >
               Інстаграм
-            </a>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
       <div className="h-full">
-        <div className="relative h-full" data-id="116647435">
-          <div
-            className="-top[92px] invisible relative block"
-            id="116647435"
-          ></div>
-          <div
-            data-testid="StyledContainer"
-            data-link
-            className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl"
-          >
-            <a
-              href="https://www.facebook.com/dom.hleba.org"
-              target="_blank"
-              rel="noopener"
-              data-testid="LinkButton"
-              className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
-              aria-describedby="profile-spasinnya "
+        <a
+          href="https://www.facebook.com/dom.hleba.org"
+          target="_blank"
+          rel="noopener"
+          data-testid="LinkButton"
+          className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
+          aria-describedby="profile-spasinnya "
+        >
+          <div className="relative h-full" data-id="116647435">
+            <div
+              className="-top[92px] invisible relative block"
+              id="116647435"
+            ></div>
+            <div
+              data-testid="StyledContainer"
+              data-link
+              className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl"
             >
               Фейсбук
-            </a>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
       <div className="h-full">
-        <div className="relative h-full" data-id="116646725">
-          <div
-            className="-top[92px] invisible relative block"
-            id="116646725"
-          ></div>
-          <div
-            data-testid="StyledContainer"
-            data-link
-            className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl"
-          >
-            <a
-              href="https://www.youtube.com/@Dim-Hliba"
-              target="_blank"
-              rel="noopener"
-              data-testid="LinkButton"
-              className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
-              aria-describedby="profile-spasinnya "
+        <a
+          href="https://www.youtube.com/@Dim-Hliba"
+          target="_blank"
+          rel="noopener"
+          data-testid="LinkButton"
+          className="sc-pFZIQ sc-idOhPF ldGKnQ jsDATa group"
+          aria-describedby="profile-spasinnya "
+        >
+          <div className="relative h-full" data-id="116646725">
+            <div
+              className="-top[92px] invisible relative block"
+              id="116646725"
+            ></div>
+            <div
+              data-testid="StyledContainer"
+              data-link
+              className="sc-bdfBwQ sc-dIUggk kStcXU kPXwbF group bl"
             >
               Ютюб
-            </a>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   );
